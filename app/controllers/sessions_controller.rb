7e65879854fcaf4db6db05bcedf462c17c3c0e0d
William Luck <luck.william.h@gmail.com>
2022-11-21 12:08:21 -0300

    add login action through sessions create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
 
+    # POST /login
     def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            # Return error as json if invalid username or password
+            render json: {errors: 'Username or password is incorrect'}
+        end
+
     end
 end
