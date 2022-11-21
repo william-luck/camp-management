dedaf2b0e40d94faeed9f17e81da5494d4258b46
William Luck <luck.william.h@gmail.com>
2022-11-21 12:03:39 -0300

    create login componenet, add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :accounts
   resources :households
   resources :beneficiaries
+
+  post '/login', to: 'sessions#create'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
