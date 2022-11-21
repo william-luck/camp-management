b7c3e26dc6125e39bf181a932aaa1b6ca588edf5
William Luck <luck.william.h@gmail.com>
2022-11-21 13:25:13 -0300

    add auto-login on page load

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Login from "./Login";
 import Form from 'react-bootstrap/Form';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function App() {
 
   const [user, setUser] = useState('')
+
+  // Checks if already logged-in user on page load
+  useEffect(() => {
+    fetch('/me').then(response => {
+      if (response.ok) {
+        response.json().then(user => setUser(user))
+      } 
+    })
+  }, [])
 
   return (
    <>
